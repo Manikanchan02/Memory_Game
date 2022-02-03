df5170ad56cdaa84af25e34ac4e36412ee6113f6
manik <manikanchanlayek055@gmail.com>
2022-02-03 21:38:24 +0530

    initial commit

---
+++ app/src/main/java/com/manikanchan/memorygame/MainActivity.kt
@@ -0,0 +1,163 @@
+package com.manikanchan.memorygame
+
+import android.animation.ArgbEvaluator
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.github.jinatonic.confetti.CommonConfetti
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener
+import com.google.android.material.snackbar.Snackbar
+import com.manikanchan.memorygame.models.BoardSize
+import com.manikanchan.memorygame.models.MemoryCard
+import com.manikanchan.memorygame.models.MemoryGame
+import com.manikanchan.memorygame.utils.DEFAULT_ICONS
+
+class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val TAG = "MainActivity"
+    }
+
+    private lateinit var clRoot: ConstraintLayout
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumMoves: TextView
+    private lateinit var tvNumPairs: TextView
+
+    private lateinit var memoryGame: MemoryGame
+    private lateinit var adapter: MemoryBoardAdapter
+    private var boardsize: BoardSize = BoardSize.EASY
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        clRoot = findViewById(R.id.clRoot)
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
+
+        setUpBoard()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.mi_refresh -> {
+                if(memoryGame.getNumMoves() > 0 && !memoryGame.haveWonGame()){
+                    showAlertDialog("Quit your current game", null, View.OnClickListener {
+                        setUpBoard()
+                    })
+                } else {
+                    setUpBoard()
+                }
+            }
+            R.id.mi_new_size -> {
+                showNewSizeDialog()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showNewSizeDialog() {
+        val boardSizeView = LayoutInflater.from(this).inflate(R.layout.dialog_board_size, null)
+        val radioGroupSize = boardSizeView.findViewById<RadioGroup>(R.id.radioGroup)
+
+        when (boardsize){
+            BoardSize.EASY -> radioGroupSize.check(R.id.rbEasy)
+            BoardSize.MEDIUM -> radioGroupSize.check(R.id.rbMedium)
+            BoardSize.HARD -> radioGroupSize.check(R.id.rbHard)
+        }
+        showAlertDialog("Choose New Size", boardSizeView, View.OnClickListener {
+            boardsize = when (radioGroupSize.checkedRadioButtonId){
+                R.id.rbEasy -> BoardSize.EASY
+                R.id.rbMedium -> BoardSize.MEDIUM
+                else -> BoardSize.HARD
+            }
+            setUpBoard()
+        })
+    }
+
+    private fun showAlertDialog(title: String, view: View?, positiveClickListener: View.OnClickListener) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setView(view)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("Ok") {_, _ ->
+                positiveClickListener.onClick(null)
+            } .show()
+    }
+
+    private fun setUpBoard() {
+        when(boardsize){
+            BoardSize.EASY -> {
+                tvNumMoves.text = "Easy: 4 x 2"
+                tvNumPairs.text = "Pairs: 0 / 4"
+            }
+            BoardSize.MEDIUM -> {
+                tvNumMoves.text = "Medium: 6 x 3"
+                tvNumPairs.text = "Pairs: 0 / 9"
+            }
+            BoardSize.HARD -> {
+                tvNumMoves.text = "Hard: 6 x 6"
+                tvNumPairs.text = "Pairs: 0 / 12"
+            }
+        }
+
+        tvNumPairs.setTextColor(ContextCompat.getColor(this, R.color.color_progress_none))
+        memoryGame = MemoryGame(boardsize)
+        adapter = MemoryBoardAdapter(this, boardsize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener{
+            override fun onCardClicked(position: Int) {
+                updateGameWithFlip(position)
+            }
+
+        })
+        rvBoard.adapter = adapter
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, boardsize.getWidth())
+    }
+
+    private fun updateGameWithFlip(position: Int) {
+        if(memoryGame.haveWonGame()){
+            Snackbar.make(clRoot, "You already won!", Snackbar.LENGTH_LONG).show()
+            return
+        }
+        if(memoryGame.isCardFaceUp(position)){
+            Snackbar.make(clRoot, "Invalid Move!", Snackbar.LENGTH_SHORT).show()
+            return
+        }
+        if (memoryGame.flipCard(position)) {
+            Log.i (TAG, "Found a match! Num pairs Found: ${memoryGame.numPairsFound}")
+            val color = ArgbEvaluator().evaluate(
+                 memoryGame.numPairsFound.toFloat() / boardsize.getNumPairs(),
+                ContextCompat.getColor(this, R.color.color_progress_none),
+                ContextCompat.getColor(this, R.color.color_progress_full)
+            ) as Int
+            tvNumPairs.setTextColor(color)
+            tvNumPairs.text = "Pairs: ${memoryGame.numPairsFound} / ${boardsize.getNumPairs()}"
+            if(memoryGame.haveWonGame()){
+                Snackbar.make(clRoot, "You won! Congratulations.", Snackbar.LENGTH_LONG).show()
+                CommonConfetti.rainingConfetti(clRoot, intArrayOf(Color.YELLOW, Color.GREEN, Color.MAGENTA)).oneShot()
+
+            }
+        }
+        tvNumMoves.text = "Moves: ${memoryGame.getNumMoves()}"
+        adapter.notifyDataSetChanged()
+    }
+}
